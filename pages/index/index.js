9b031c6bacff2e9cd42f59e1b33539300e515076
aiff <aiff@outlook.com>
2016-10-22 10:45:50 +0800

    new a connoit

---
+++ pages/index/index.js
@@ -0,0 +1,35 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  
+  data: {
+    // diss:true,
+    len:2,
+    name:'qq',
+    //  array:[1,2,3,4,'hello'],
+  },
+  inputfn:function(e){
+      console.log(e);
+
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },doload :function(){
+     //this.setdata:true
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  }
+})
